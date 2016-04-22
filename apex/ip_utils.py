8a2189163b03db86c2096c7b0f375e96fecc8771
Feng Pan <fpan@redhat.com>
2016-04-21 21:08:40 -0400

    initial commit

---
+++ apex/ip_utils.py
@@ -0,0 +1,184 @@
+##############################################################################
+# Copyright (c) 2016 Feng Pan (fpan@redhat.com)
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+
+import ipaddress
+import subprocess
+import re
+import logging
+
+
+def get_ip_range(start_offset=None, count=None, end_offset=None,
+                 cidr=None, interface=None):
+    if cidr:
+        if count and start_offset and not end_offset:
+            start_index = start_offset
+            end_index = start_offset + count
+        elif count and end_offset and not start_offset:
+            end_index = -1 - end_offset
+            start_index = end_index - count
+        elif start_offset and end_offset and not count:
+            start_index = start_offset
+            end_index = -1 - end_offset
+        else:
+            raise IPUtilsException("Argument error: must pass in exactly 2 of"
+                                   "start_offset, end_offset and count")
+
+        start_ip = cidr[1 + start_index]
+        end_ip = cidr[end_index]
+        network = cidr
+    elif interface:
+        network = interface.network
+        number_of_addr = network.num_addresses
+        if interface.ip < network[int(number_of_addr / 2)]:
+            if count and start_offset and not end_offset:
+                start_ip = interface.ip + start_offset
+                end_ip = start_ip + count
+            elif count and end_offset and not start_offset:
+                end_ip = network[-1 - end_offset]
+                start_ip = end_ip - count
+            elif start_offset and end_offset and not count:
+                start_ip = interface.ip + start_offset
+                end_ip = network[-1 - end_offset]
+            else:
+                raise IPUtilsException(
+                    "Argument error: must pass in exactly 2 of"
+                    "start_offset, end_offset and count")
+        else:
+            if count and start_offset and not end_offset:
+                start_ip = network[1 + start_offset]
+                end_ip = start_ip + count
+            elif count and end_offset and not start_offset:
+                end_ip = interface.ip - end_offset
+                start_ip = end_ip - count
+            elif start_offset and end_offset and not count:
+                start_ip = network[1 + start_offset]
+                end_ip = interface.ip - end_offset
+            else:
+                raise IPUtilsException(
+                    "Argument error: must pass in exactly 2 of"
+                    "start_offset, end_offset and count")
+
+    else:
+        raise IPUtilsException("Must pass in cidr or interface to generate"
+                               "ip range")
+
+    range_result = validate_ip_range(start_ip, end_ip, network)
+    if range_result == 0:
+        ip_range = "{},{}".format(start_ip, end_ip)
+        return ip_range
+    else:
+        raise IPUtilsException(range_result)
+
+
+def get_ip(offset, cidr=None, interface=None):
+    if cidr:
+        ip = cidr[0 + offset]
+        network = cidr
+    elif interface:
+        ip = interface.ip + offset
+        network = interface.network
+    else:
+        raise IPUtilsException("Must pass in cidr or interface to generate IP")
+
+    if ip not in network:
+        raise IPUtilsException("IP {} not in network {}".format(ip, network))
+    else:
+        return str(ip)
+
+
+def generate_ip_range(args):
+    """
+    Generate IP range in string format for given CIDR.
+    This function works for both IPv4 and IPv6.
+
+    args is expected to contain the following members:
+    CIDR: any valid CIDR representation.
+    start_position: starting index, default to first address in subnet (1)
+    end_position:  ending index, default to last address in subnet (-1)
+
+    Returns IP range in string format. A single IP is returned if start and
+    end IPs are identical.
+    """
+    cidr = ipaddress.ip_network(args.CIDR)
+    (start_index, end_index) = (args.start_position, args.end_position)
+    if cidr[start_index] == cidr[end_index]:
+        return str(cidr[start_index])
+    else:
+        return ','.join(sorted([str(cidr[start_index]), str(cidr[end_index])]))
+
+
+def get_interface(nic, address_family=4):
+    """Returns interface object for a given NIC name in the system"""
+    if not nic.strip():
+        logging.error("empty nic name specified")
+        return None
+    output = subprocess.getoutput("ip -{} addr show {} scope global"
+                                  .format(address_family, nic))
+    if address_family == 4:
+        pattern = re.compile("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}")
+    elif address_family == 6:
+        pattern = re.compile("([0-9a-f]{0,4}:){2,7}[0-9a-f]{0,4}/\d{1,3}")
+    else:
+        raise IPUtilsException("Invalid address family: {}"
+                               .format(address_family))
+    match = re.search(pattern, output)
+    if match:
+        logging.info("found interface {} ip: {}".format(nic, match.group()))
+        return ipaddress.ip_interface(match.group())
+    else:
+        logging.warning("interface ip not found! ip address output:\n{}"
+                        .format(output))
+        return None
+
+
+def find_gateway(interface):
+    """Validate gateway on the system"""
+
+    address_family = interface.version
+    output = subprocess.getoutput("ip -{} route".format(address_family))
+
+    pattern = re.compile("default\s+via\s+(\S+)\s+")
+    match = re.search(pattern, output)
+
+    if match:
+        gateway_ip = match.group(1)
+        reverse_route_output = subprocess.getoutput("ip route get {}"
+                                                    .format(gateway_ip))
+        pattern = re.compile("{}.+src\s+{}".format(gateway_ip, interface.ip))
+        if not re.search(pattern, reverse_route_output):
+            logging.warning("Default route doesn't match iterface specified: {}"
+                            .format(reverse_route_output))
+            return None
+        else:
+            return gateway_ip
+    else:
+        logging.warning("Can't find gateway address on system")
+        return None
+
+
+def validate_ip_range(start_ip, end_ip, cidr):
+    ip_range = "{},{}".format(start_ip, end_ip)
+    if end_ip <= start_ip:
+        return ("IP range {} is invalid: end_ip should be greater than starting"
+                " ip".format(ip_range))
+    if start_ip not in ipaddress.ip_network(cidr):
+        return 'start_ip {} is not in network {}'.format(start_ip, cidr)
+    if end_ip not in ipaddress.ip_network(cidr):
+        return 'end_ip {} is not in network {}'.format(end_ip, cidr)
+
+    return 0
+
+
+class IPUtilsException(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
---
+++ apex/net_env.py
@@ -0,0 +1,180 @@
+##############################################################################
+# Copyright (c) 2016 Feng Pan (fpan@redhat.com)
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+
+import yaml
+import logging
+import ipaddress
+import apex.ip_utils as ip_utils
+
+
+ADMIN_NETWORK = 'admin_network'
+PRIVATE_NETWORK = 'private_network'
+PUBLIC_NETWORK = 'public_network'
+STORAGE_NETWORK = 'storage_network'
+API_NETWORK = 'api_network'
+OPNFV_NETWORK_TYPES = [ADMIN_NETWORK, PRIVATE_NETWORK, PUBLIC_NETWORK,
+                       STORAGE_NETWORK, API_NETWORK]
+net_isolation_enabled = True
+
+
+class NetworkSettings:
+    def __init__(self, filename, network_isolation):
+        with open(filename, 'r') as network_settings_file:
+            self.settings_obj = yaml.load(network_settings_file)
+            self.network_isolation = network_isolation
+            self.enabled_network_list = []
+            self._validate_input()
+
+    def _validate_input(self):
+        if ADMIN_NETWORK not in self.settings_obj or \
+                self.settings_obj[ADMIN_NETWORK].get('enabled') != True:
+            raise NetworkSettingsException("You must enable admin_network "
+                                           "and configure it explicitly or "
+                                           "use auto-detection")
+        if self.network_isolation and \
+            (PUBLIC_NETWORK not in self.settings_obj or
+                self.settings_obj[PUBLIC_NETWORK].get('enabled') != True):
+            raise NetworkSettingsException("You must enable public_network "
+                                           "and configure it explicitly or "
+                                           "use auto-detection")
+
+        for network in OPNFV_NETWORK_TYPES:
+            if network in self.settings_obj:
+                if self.settings_obj[network].get('enabled') == True:
+                    logging.info("{} enabled".format(network))
+                    self._config_required_settings(network)
+                    self._config_ip_range(network=network,
+                                          setting='usable_ip_range',
+                                          start_offset=20, end_offset=22)
+                    self._config_optional_settings(network)
+                    self.enabled_network_list.append(network)
+                else:
+                    logging.info("{} disabled, will collapse with "
+                                 "admin_network".format(network))
+            else:
+                logging.info("{} is not in specified, will collapse with "
+                             "admin_network".format(network))
+
+    def _config_required_settings(self, network):
+        """Configures either CIDR or bridged_interface setting"""
+        cidr = self.settings_obj[network].get('cidr')
+        nic_name = self.settings_obj[network].get('bridged_interface')
+
+        if cidr:
+            cidr = ipaddress.ip_network(self.settings_obj[network]['cidr'])
+            self.settings_obj[network]['cidr'] = cidr
+            logging.info("{}_cidr: {}".format(network, cidr))
+            return 0
+        elif nic_name:
+            # If cidr is not specified, we need to know if we should find
+            # IPv6 or IPv4 address on the interface
+            if self.settings_obj[network].get('ipv6') == True:
+                address_family = 6
+            else:
+                address_family = 4
+            nic_interface = ip_utils.get_interface(nic_name, address_family)
+            if nic_interface:
+                self.settings_obj[network]['bridged_interface'] = nic_interface
+                logging.info("{}_bridged_interface: {}".
+                             format(network, nic_interface))
+                return 0
+            else:
+                raise NetworkSettingsException("Auto detection failed for {}: "
+                                               "Unable to find valid ip for "
+                                               "interface {}"
+                                               .format(network, nic_name))
+
+        else:
+            raise NetworkSettingsException("Auto detection failed for {}: "
+                                           "either bridge_interface or cidr "
+                                           "must be specified"
+                                           .format(network))
+
+    def _config_ip_range(self, network, setting, start_offset=None,
+                         end_offset=None, count=None):
+        ip_range = self.settings_obj[network].get(setting)
+        interface = self.settings_obj[network].get('bridged_interface')
+
+        if not ip_range:
+            cidr = self.settings_obj[network].get('cidr')
+            ip_range = ip_utils.get_ip_range(start_offset=start_offset,
+                                             end_offset=end_offset,
+                                             count=count,
+                                             cidr=cidr,
+                                             interface=interface)
+            self.settings_obj[network][setting] = ip_range
+
+        logging.info("{}_{}: {}".format(network, setting, ip_range))
+
+    def _config_optional_settings(self, network):
+        if network == ADMIN_NETWORK:
+            self._config_ip(network, 'provisioner_ip', 1)
+            self._config_ip_range(network=network, setting='dhcp_range',
+                                  start_offset=1, count=8)
+            self._config_ip_range(network=network,
+                                  setting='introspection_range',
+                                  start_offset=10, count=8)
+        elif network == PUBLIC_NETWORK:
+            self._config_ip(network, 'provisioner_ip', 1)
+            self._config_ip_range(network=network,
+                                  setting='floating_ip',
+                                  end_offset=2, count=20)
+            self._config_gateway(network)
+
+    def _config_gateway(self, network):
+        gateway = self.settings_obj[network].get('gateway')
+        interface = self.settings_obj[network].get('bridged_interface')
+
+        if not gateway:
+            cidr = self.settings_obj[network].get('cidr')
+            if cidr:
+                gateway = ip_utils.get_ip(1, cidr)
+            else:
+                gateway = ip_utils.find_gateway(interface)
+
+            if gateway:
+                self.settings_obj[network]['gateway'] = gateway
+            else:
+                raise NetworkSettingsException("Failed to set gateway")
+
+        logging.info("{}_gateway: {}".format(network, gateway))
+
+    def _config_ip(self, network, setting, offset):
+        ip = self.settings_obj[network].get(setting)
+        interface = self.settings_obj[network].get('bridged_interface')
+
+        if not ip:
+            cidr = self.settings_obj[network].get('cidr')
+            ip = ip_utils.get_ip(offset, cidr, interface)
+            self.settings_obj[network][setting] = ip
+
+        logging.info("{}_{}: {}".format(network, setting, ip))
+
+    def dump_yaml(self):
+        print(yaml.dump(self.settings_obj, default_flow_style=False))
+
+    def dump_bash(self, path=None):
+        bash_str = ''
+        for network in self.enabled_network_list:
+            for key, value in self.settings_obj[network].items():
+                bash_str += "{}_{}={}\n".format(network, key, value)
+        if path:
+            with open(path, 'w') as file:
+                file.write(bash_str)
+        else:
+            print(bash_str)
+
+
+class NetworkSettingsException(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+            return self.value
